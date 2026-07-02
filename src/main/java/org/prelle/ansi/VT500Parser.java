958010101097b35bdfcca52356880173c954ab35
Stefan Prelle <stefan@rpgframework.de>
2026-07-02 22:03:14 +0200

    Fix detection for RIP query *with* parameter

--- src/main/java/org/prelle/ansi/VT500Parser.java
+++ src/main/java/org/prelle/ansi/VT500Parser.java
@@ -254,6 +254,7 @@ public class VT500Parser {
 		case CSI_PARAM:
 			switch ( (Integer)code) {
 			case Integer x when isExecutableC0(codeF) -> callback.execute( C0Code.valueOf(code));
+			case Integer x when x==0x21 -> {csiDispatch( (char)(int)x); enterState(ParserState.GROUND);} // Workaround for RIPScrip, because they use 0x21 as a final char
 			case Integer x when x>=0x20 && x<=0x2F -> {collect(code); enterState(ParserState.CSI_INTERMEDIATE);}
 			case Integer x when x>=0x30 && x<=0x3F -> param(code);
 //			case Integer x when x==0x3A ->  enterState(ParserState.CSI_IGNORE);
